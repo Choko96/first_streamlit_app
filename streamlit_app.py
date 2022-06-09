ca3b36aefdd193e8f2fb988a73e0127abd66c65b
Adonis <adonis2017.ac@gmail.com>
2022-06-09 11:07:35 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into fruit_load_list values (?)", fruit_choiceb)
 
-my_cur.execute("select * from fruit_load_list where ")
+#my_cur.execute("select * from fruit_load_list where ")
 
 my_data_row = my_cur.fetchall()
 
